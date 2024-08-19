782b55154332b0be2b52b34d8ce8d4231381c28a
Najil Parakkal <najilparakkal@gmail.com>
2024-08-19 15:25:10 +0530

    host

--- src/config/express.ts
+++ src/config/express.ts
@@ -5,7 +5,7 @@ const cors = require("cors");
 import { connectDb } from "./database";
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: process.env.ORIGIN,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   credentials: true, 
 };
