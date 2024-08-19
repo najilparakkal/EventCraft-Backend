f46055b7ba357bb43303be17b71e04820ce4981d
najilparakkal <141852654+najilparakkal@users.noreply.github.com>
2024-08-19 15:19:48 +0530

    Delete dist directory

--- dist/utils/otp.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateOTP = void 0;
-const util_functions_nodejs_1 = require("util-functions-nodejs");
-const generateOTP = () => (0, util_functions_nodejs_1.generateOtp)(4).toString();
-exports.generateOTP = generateOTP;
